ea4f9242126202772cc58b6060854d484259eda1
tewinsid <vipwanggang@outlook.com>
2018-06-26 18:09:25 +0800

    '20180626-1'

---
+++ aircraftWarMy/js/main.js
@@ -0,0 +1,10 @@
+//程序入口
+var __main = funciton () {
+  var images = [
+    '/img/backgroud_gray.png',
+    '/img/bullet.png',
+    '/img/enemy1.png',
+    '/img/hero.png',
+  ]
+  var game = GuaGame()
+}
