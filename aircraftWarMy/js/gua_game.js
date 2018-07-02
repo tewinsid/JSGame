b07d7ce4cb5c08a9dd4f35f6d41da945b74b52f7
tewinsid <vipwanggang@outlook.com>
2018-07-02 18:36:11 +0800

    '20180702-temp1'

--- aircraftWarMy/js/gua_game.js
+++ aircraftWarMy/js/gua_game.js
@@ -1,58 +1,4 @@
-// class GuaGame {
-//   constructor (fps, images) {
-//     this.images = images;
-//     window.fps = fps;
-//     this.actions = {};
-//     this.keydowns = {};
-//     this.scene = null;
-//     this.canvas = document.getElementById('canvas');
-//     this.context = this.canvas.getContext('2d');
-//     init();
-//   }
-//   //游戏， canvas相当于画布，通过不断在画布上绘图实现游戏效果
-//   //交互， 通过按键和鼠标点击实现交互，以事件的形式注册到game对象上
-//   //通过定时器实现不断执行绘制代码
-//   //首先要加载图片
-//   //draw方法 确定要绘制的元素
-//   //update，更新绘制元素状态
-//   //runloop方法 定时器循环调用update draw方法
-//
-//   init () {
-//     let g = this;
-//     var loads = [];
-//     var names = Object.keys(g.images);
-//     for (var i = 0; i < names.length; i++) {
-//       let name = names[i];
-//       let path = images[name];
-//       Image img = new Image()
-//       img.src = path
-//       img.onload = function () {
-//         loads.push[1];
-//         if(loads.length == names.length) {
-//           g.__start()
-//         }
-//       }
-//     }
-//   }
-//
-//   update () {
-//     this.scene.update();
-//   }
-//
-//   draw () {
-//     this.scene.draw();
-//   }
-//
-//   runloop () {
-//     var actions =
-//   }
-//
-//   drawImage (img) = {
-//     this.context.drawImage(img, img.widht, img.height);
-//   }
-// }
-
-var log = function (msg) {
+var log = function(msg) {
   console.log(msg);
 }
 var GuaGame = function() {
@@ -60,6 +6,7 @@ var GuaGame = function() {
     actions: {},
     keydowns: {},
     pause: false,
+    bullets: [],
   };
   g.canvas = document.getElementById('canvas')
   g.context = g.canvas.getContext('2d');
@@ -77,12 +24,12 @@ var GuaGame = function() {
 
   //timer
   setInterval(function() {
-    if(!g.pause) {
+    if (!g.pause) {
       g.update();
-      g.context.clearRect(0,0,canvas.width,canvas.height);
+      g.context.clearRect(0, 0, canvas.width, canvas.height);
       g.draw();
     }
-  }, 1000/30);
+  }, 1000 / 30);
   return g;
 }
 var imageFromPaht = function(path) {
@@ -104,22 +51,22 @@ var Hero = function(game) {
     o.height = o.image.height;
   }
   o.moveLeft = function() {
-    if(o.x >= 0) {
+    if (o.x >= 0) {
       o.x -= o.speed;
     }
   }
   o.moveRight = function() {
-    if(o.x <= (game.canvas.width - o.width)) {
+    if (o.x <= (game.canvas.width - o.width)) {
       o.x += o.speed;
     }
   }
   o.moveUp = function() {
-    if(o.y >= 0) {
+    if (o.y >= 0) {
       o.y -= o.speed;
     }
   }
   o.moveDown = function() {
-    if(o.y <= game.canvas.height - o.height) {
+    if (o.y <= game.canvas.height - o.height) {
       o.y += o.speed;
     }
   }
@@ -130,30 +77,36 @@ var Bullet = function(hero) {
   var o = {
     image: image,
   }
-  image.onload = function(){
+  image.onload = function() {
     o.width = o.image.width;
     o.height = o.image.height;
     o.x = hero.x + ((hero.width - o.width) / 2);
     o.y = hero.y - 25;
   }
+  o.move = function() {
+    o.y -= 5;
+  }
+  return o;
+}
+var Background = function() {
+  var image = imageFromPaht('./img/background_gray.png');
+  var o = {
+    image: image,
+  }
   return o;
 }
 var __main = function() {
   var game = GuaGame();
-  var background = new Image();
-  //怎样处理onload
-  background.src = './img/background_gray.png';
+  var background = Background();
   var hero = Hero(game);
-  var bullets = [];
-  //
+  var bullets = game.bullets;
   registerAllAction(game, hero);
-  //变相实现注册 就是在对象外面更改对象方法
   setInterval(function() {
     var bullet = Bullet(hero);
     bullets[bullets.length] = bullet;
-  }, 1000/5);
+  }, 1000 / 5);
   window.addEventListener('keydown', function(event) {
-    if(event.key == 'p') {
+    if (event.key == 'p') {
       game.pause = !game.pause;
     }
   })
@@ -168,25 +121,24 @@ var __main = function() {
     }
     for (var i = 0; i < bullets.length; i++) {
       var bulletTemp = bullets[i];
-      bulletTemp.y -= 5;
-      if(bulletTemp.y <= 0) {
-        deleteBullet(i , bullets);
+      bulletTemp.move();
+      if (bulletTemp.y <= 0) {
+        deleteBullet(i, bullets);
       }
     }
   }
   game.draw = function() {
-    game.context.drawImage(background, 0, 0);
+    game.context.drawImage(background.image, 0, 0);
     game.context.drawImage(hero.image, hero.x, hero.y);
     for (var i = 0; i < bullets.length; i++) {
       var bulletTemp = bullets[i];
       game.context.drawImage(bulletTemp.image, bulletTemp.x, bulletTemp.y)
     }
   }
-
 }
 var deleteBullet = function(index, bullets) {
-  for(var i=index; i < bullets.length; i++) {
-    bullets[i] = bullets[i+1];
+  for (var i = index; i < bullets.length; i++) {
+    bullets[i] = bullets[i + 1];
   }
   bullets.length--;
 }
